74c20435177bc187585106bfc1284c4579f5cfee
Ermanno Goletto <e.goletto@outlook.it>
2017-04-30 18:26:56 +0200

    Add files via upload

--- 01-CreateSelfSignedCert-Test.cmd
+++ 01-CreateSelfSignedCert-Test.cmd
@@ -10,8 +10,20 @@ REM *** Configurazioni ***
 SET OpenSSBinPath=%~dp0OpenSSL\OpenSSL-Win64-1_1_0e\bin
 SET CertsPath=%~dp0Certs
 
+CLS
+@ECHO OFF
+
+ECHO Creazione certificato RSA autofirmato %FileName%
+ECHO Key=%KeyLenght% bits, SHA=%SHABits% bits , Validity=%ValidityDays% days
+ECHO.
+
+IF EXIST %CertsPath%\%FileNAme%.key CHOICE /C:SN /M "Sovrascrivere il certificato esistente?"
+IF ERRORLEVEL 2 GOTO END
+
 %OpenSSBinPath%\openssl req -x509 -sha%SHABits% -days %ValidityDays% -newkey rsa:%KeyLenght% -keyout %CertsPath%\%FileName%.key -out %CertsPath%\%FileName%.crt -passout pass:%Password% -subj %Subject% -config %OpenSSBinPath%\openssl.cfg
 
 %OpenSSBinPath%\openssl rsa -in %CertsPath%\%FileName%.key -out %CertsPath%\%FileName%-nopass.key -passin pass:%Password%
 
-pause
+PAUSE
+
+:END
